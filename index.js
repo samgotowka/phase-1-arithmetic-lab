236f3039daf25a8a96e6db4bab9b8a30d4c1cf16
Sam Gotowka <sam.gotowka@gmail.com>
2022-07-11 17:29:57 -0400

    commit tree 27e04e1044377ebbcb37bfbd9dfdaf490374591b

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
-// Write your code here
+let num1 =2
+let num2 = 31
+let num3 = 22
+let num4 = 6
+function multiply(num1,num2) {
+    return (num1 * num2);
+}
+function random() {
+    return Math.random
+}
+function mod(num3,num4) {
+    console.log(num3 %num4)
+}
